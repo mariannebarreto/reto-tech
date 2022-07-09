591f7d0ef63633d8f67bbbe965ffd0c59a1f877b
mariannebarreto <barretoink@icloud.com>
2022-07-09 08:49:10 -0500

    Cierre reto técnico: faltó infinite-scroll

---
+++ src/app/components/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-container',
+  template:`<div>IMG</div>`,
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  title = 'card';
+}
