669f406adaa17048cc9185ba0ca35f125a7d6991
mariannebarreto <barretoink@icloud.com>
2022-07-08 23:44:55 -0500

    Set up listo para comenzar

---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-container',
+    template: `<h1>Container component</h1>`,
+    
+})
+export class ContainerComponent{
+
+}
